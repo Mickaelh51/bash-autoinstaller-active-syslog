03dd90cbf184f6247c1746ca2dce23d24d0eb04f
mickaelh51 <mickael@winlux.fr>
2015-10-04 21:25:43 +0200

    add changes

--- check_bash_version.sh
+++ check_bash_version.sh
@@ -3,14 +3,14 @@
 #Add this script in all servers
 #Add this line in snmpd.conf
 #exec bashversion /usr/local/libexec/check_bash_version.sh # .1.3.6.1.4.1.2021.8.1.101.1
-#Nagios command is : check_voip_bash_version with just one argument: OID
+#Nagios command can be : check_bash_version with just one argument: OID
 
 VER=`bash --version`
 STATE_OK=0
 STATE_CRITICAL=2
 
 
-#VER="non"
+#VER="test"
 
 if [[ $VER =~ "GNU bash (MIKA)" ]]
 then
